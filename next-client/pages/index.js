d3ecc3bec83917032c3f9ef84047577ebbdb7c5b
Yang Li <lixzone_liyang@hotmail.com>
2022-01-15 14:55:52 -0500

    Working Copy of Github Action Building and deploying docker file to Google Cloud using Secret Key

--- next-client/pages/index.js
+++ next-client/pages/index.js
@@ -1,28 +1,34 @@
 import useSWR from 'swr'
 import Person from '../components/Person'
-import React, { useRef, useState, Suspense } from 'react'
+import React, { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import * as THREE from 'three'
-import App from './App'
+
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
-const pexel = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
-const images = [
-  // Front
-  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970) },
-  // Back
-  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
-  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
-  // Left
-  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482) },
-  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185) },
-  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574) },
-  // Right
-  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675) },
-  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738) },
-  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986) }
-]
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 3 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
 
 export default function Index() {
   const { data, error } = useSWR('/api/people', fetcher)
@@ -30,15 +36,15 @@ export default function Index() {
   if (error) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
 
+  let windowWidth = document.body.clientWidth;
+  let windowHeight = document.body.clientHeight;
+  
   return (
-    <Suspense fallback={null}>
-      <App images={images} />
-    </Suspense>
-    // <Canvas>
-    //   <ambientLight />
-    //   <pointLight position={[10, 10, 10]} />
-    //   <Box position={[1.2, 0, 0]} />
-    // </Canvas>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
     
   )
 }
